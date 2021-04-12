2a84e54027172df9f3654c9080c498648bc6f520
Rahul Gorai <rahul.gorai.3141@gmail.com>
2021-04-12 22:36:30 +0530

    Add major frontend components

---
+++ src/drawer.js
@@ -0,0 +1,270 @@
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Typography from '@material-ui/core/Typography';
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import './App.css';
+
+function Drawer() {
+  const defaultValues = {
+    name: "",
+    age: 0,
+    gender: "",
+    os: "",
+    favoriteNumber: 0,
+  };
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <Grid container alignItems="center" justify="center" direction="column">
+          <Grid item style={{marginTop: 10}}>
+            <Typography variant="h4">Member 1</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item style={{marginTop: 10}}>
+            <Typography variant="h4">Member 2</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item style={{marginTop: 10}}>
+            <Typography variant="h4">Member 3</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item style={{marginTop: 10}}>
+            <Typography variant="h4">Member 4</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item style={{marginTop: 10}}>
+            <Typography variant="h4">Member 5</Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Button variant="contained" color="primary" type="submit" style={{ marginTop: 10 }}>
+            Submit
+        </Button>
+        </Grid>
+      </form>
+    </div>
+  );
+}
+
+export default Drawer;
